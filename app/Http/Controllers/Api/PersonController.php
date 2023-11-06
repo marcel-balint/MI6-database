b3c7b399e740a84b5b2ec33605e493b4f8577abc
Daniel Slezak <danielslezaak@gmail.com>
2023-11-06 15:28:57 +0100

    for data

---
+++ app/Http/Controllers/Api/PersonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+use function Laravel\Prompts\select;
+
+class PersonController extends Controller
+{
+    public function index() {
+        $people = Person::get();
+        dd($people);
+    }
+}
