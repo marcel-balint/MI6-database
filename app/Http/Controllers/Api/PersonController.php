b2df55ce4e8d8401c3a77f403dba3a469f4534dc
Marcel Balint <marcelbalint363@gmail.com>
2023-11-06 17:41:20 +0200

    fetch agent by id

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -10,11 +10,17 @@ use function Laravel\Prompts\select;
 
 class PersonController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $people = Person::with('aliases', 'image')->limit(20)->get();
 
         return $people;
     }
+    public function show(string $id)
+    {
+        // it has GET by default so we start with 'with'
+        $the_agent = Person::with('aliases', 'image')->findOrFail($id);
 
-
+        return $the_agent;
+    }
 }
