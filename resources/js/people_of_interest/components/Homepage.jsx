86aa77cd79f7ad760a8c63043064ec04854fc8ef
Artem <antonenkoartem0504@gmail.com>
2023-11-06 15:28:35 +0100

    moving panel

--- resources/js/people_of_interest/components/Homepage.jsx
+++ resources/js/people_of_interest/components/Homepage.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '/resources/css/style.scss'
+import Sidepanel from './Sidepanel';
 export default function Homepage() {
 
-    // let sidenav = document.querySelector('.sidenav');
-
-    // let arrow_span = sidenav.querySelector('.sidenav__arrow');
-
-    // arrow_span.addEventListener('click', () => {
-
-    //     sidenav.classList.toggle('sidenav_closed')
-    //     arrow_span.classList.toggle('sidenav__arrow_closed')
-    // })
+    const [open, setOpen] = useState(1)
 
     return (
         <div className='body'>
             <nav className="sidenav">
-            <div className="sidenav__content">
-                <img src="https://classes.codingbootcamp.cz/assets/classes/1404/mi6-seal.png" alt="seal" className="sidenav__image"/>
-                <ul className="sidenav__links">
-                <li className="sidenav__link sidenav__link_active"><a href="#">Home</a></li>
-                <li className="sidenav__link"><a href="#">People of interest</a></li>
-                </ul>
-            </div>
-            <span className="sidenav__arrow"></span>
+                <div className="sidenav__content">
+                    <img src="https://classes.codingbootcamp.cz/assets/classes/1404/mi6-seal.png" alt="seal" className="sidenav__image"/>
+
+                    <ul className="sidenav__links">
+
+                        <li className="sidenav__link sidenav__link_active"><a href="#">Home</a></li>
+
+                        <li className="sidenav__link"><a href="#">People of interest</a></li>
+
+                    </ul>
+
+                </div>
+
+                <button className="sidenav__arrow" onClick={ () => { setOpen (open - 1) }}></button>
+                <span className="sidenav__arrow" onClick={ () => { setOpen (open + 1) }}></span>
             </nav>
 
             <main>
-            <h1>Welcome to MI6</h1>
+                <h1>Welcome to MI6</h1>
             </main>
 
         </div>
