ca49ab9507af9323fcfdf38ad0fb63c9193da2d8
Marcel Balint <marcelbalint363@gmail.com>
2023-11-07 17:32:35 +0200

    Send form data

--- app/Http/Controllers/Api/MissionController.php
+++ app/Http/Controllers/Api/MissionController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class MissionController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $missions = Mission::get();
 
         return $missions;
@@ -17,7 +18,20 @@ class MissionController extends Controller
     public function show($mission_id)
     {
         $mission = Mission::findOrFail($mission_id);
-        
+
         return $mission;
+    }
+
+
+    public function store(Request $request, $id)
+    {
+        $mission = Mission::findOrFail($id);
+        $mission->name = $request->input('name');
+        $mission->year = $request->input('year');
+        $mission->outcome = $request->input('outcome');
+        $mission->save();
+        return [
+            'status' => 'success'
+        ];;
     }
 }
